531ff4d16d021f284314eea0caacbb35c96670d2
Xartd0 <43171120+xartd0@users.noreply.github.com>
2022-02-11 03:12:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 import base64
-from  more_itertools import unique_everseen
-from colorama import init, Fore
-init(autoreset=True,convert=True)
+import re
 
 #auth_token аккаунта буду менять,но он может перестать работать,поэтому лучше используйте свой
 # еще один токен Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2NDM2NTE3NDksImV4cCI6MTY0NjI0Mzc0OSwicm9sZXMiOlsiUk9MRV9FRFVfU0tZU01BUlRfU1RVREVOVF9VU0FHRSJdLCJ1c2VySWQiOjQ4OTg1Mzg1LCJlbWFpbCI6ImdkZmtqbmdrc2hqZG1mbmdraGpzbkBnbWFpbC5jb20iLCJuYW1lIjoi0JLQsNC90Y8iLCJzdXJuYW1lIjoi0JLQsNC90Y8iLCJpZGVudGl0eSI6Im11YmluaW5vZ2kifQ.CDpO-n7febtcZErWOFmK9R6WmJc7a2uNXDYBRbOvJetVdMlhXAGaj3nb3yS6rUMxRnEZcBuNcFeroI5F9yUAMk5uMIGEaTlECiMIxWLE4VUZ2TRkHK_w6jNlRZC3RBHBJjISWdFpA3i37pMarpij7tHnNNzN2DSvjzL3KNLe_Hy1mliN_LFZGRg2UNwRvc77u71zxI7ExIZ2-MwVjY53OoHxiqVZr-59y3rhnMLtREQGsCBmHZXkuf9rlv9wZxsipF9y_w03SMFGZjhXvchIVuGiJbDD11bVPA1LXHO5w2H71WXTn05ZV3Z6YG11s8Dn-AaPx2uKCW4QYsQDwpxGZ-fNRxP36Qb1vgQpMoCKcSpjV3bH7hPyzTgxqWNCBX7oYv2OTvCU9GMiN1T7nGc6kLI5SF-C_PvpMu1LkKwK35MgaLRVt4Ffjvh9ujKTzVHzEJuBzak9R-8MWTC8Sq-Olr3Hmb7KWnT9-egTsgLs6kXURqCfaFAupOAdd5ABhaK9KwsfAvp5zpNeP-f_oG59iH5d7atUKtC5I4hgtchXyvqet88b6Gu0ZO49akXteVSI0yReA_kQUlx0hEcQQx1tDM9lF2DM3pexDLE1ZGBm8b2O7-cWl7GSIpaM-zPXVz16NMNrgqoperSLmEEBWtE3cNhvbJJMEu0esu5JhNYTE_g
 auth_token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2NDM2NTEzNjAsImV4cCI6MTY0NjI0MzM2MCwicm9sZXMiOlsiUk9MRV9FRFVfU0tZU01BUlRfU1RVREVOVF9VU0FHRSJdLCJ1c2VySWQiOjQ4OTgzMzU3LCJlbWFpbCI6Im9hZGptZmdram5hZGZrbWdqQGdtYWlsLmNvbSIsIm5hbWUiOiLQktC70LDQtCDQktC70LDQtCIsInN1cm5hbWUiOiLQktC70LDQtCIsImlkZW50aXR5IjoieGl6dXJldGlkdSJ9.E3wX-TAtl05HshNr-DS2ktcpYsie_Stv_zkxeY2oPzU98kF146N2p5ubRbyL87Qn53eIBu-VFBDxIK3aqI52nD2gZtigG5eU58svtbDBDSiWtHT0wNclFhtxAhZtXF6tP7TJXZ53O-vhAiZTyRwA4yODGUiIeQ7Fdv3oluvDzg1YQO4ctJBXrQSffJ_P9eEQW_4X7y5ccn3BMD52iA6tgj86mAcrw87iWRC1WSxkpCk03L8Hpary7Lqs7etFt0Hc7nPrdbra0czeT1d6SZaxtFA_6e0Eci0qT8CGBQ_kkyLU8K8oxdrd1BL5VLteH41Znsa_tsHEifIwUoCSwp_Qc6dkDNZXmPqWzWadaz6LeZJRxu81V4Y-N8rNMP5-XnC6BkRp5EaBDAnlcF9OyjjdgSLqMyO8BDEzdN4Aqg6gwyQ5N002EBpxlUmaBBPwrwBp4_b1TZFPGzbPamnt_JCM4-muylWln55Rud13yjRINPz8RO-zvbv4XFRpfGA-ffhVQec8GyvIVNQJahEDl8jVnLRJxgImb8lobH6eNC4bNbqzuSbkH3qSyVbeXUa2XZEyp2CMJ9mA6I041uONiOCjhvGy7dUq61lc3zwhIm0bS7AND2-6M8wvdGXowGgIynBAGnWuLWrOpEVWit5ygC-JOy6E9H2cC8CBd4YrViUDmlM'
-results =[]
 
 def answerparse(taskHash):
+    x = 0
+    results = []
     # ---- получение uuid заданий в тесте ----#
     url = f"https://api-edu.skysmart.ru/api/v1/task/start"
     payload = "{\"taskHash\":\"" + taskHash + "\"}"
@@ -37,6 +37,7 @@ def answerparse(taskHash):
     checkradom = steps_raw['taskStudentMeta']['steps'] # все uuid заданий
     random = False # проверка на рандомные задания
     for uuid in checkradom:
+        x = x + 1
         url = "https://api-edu.skysmart.ru/api/v1/content/step/load?stepUuid=" + uuid['stepUuid']
         headers = {
         'Authorization': auth_token
@@ -45,13 +46,17 @@ def answerparse(taskHash):
         answer_row = response.json()
         soup = BeautifulSoup(answer_row['content'], 'html.parser')
         try:
-            anstitlerow = '📝Вопрос:' + (soup.find('vim-instruction').text.replace('\n', ' ')).replace('\r',' ')
+            anstitlerow = f'№{x}📝Вопрос: ' + (soup.find('vim-instruction').text.replace('\n', ' ')).replace('\r',' ')
             results.append(anstitlerow)
         except:
-            anstitlerow = '📝Вопрос:' + (soup.find('vim-content-section-title').text.replace('\n', ' ')).replace('\r',' ')
+            anstitlerow = f'№{x}📝Вопрос: ' + (soup.find('vim-content-section-title').text.replace('\n', ' ')).replace('\r',' ')
             results.append(anstitlerow)
         # ledotetote
         # а тут много циклов,каждый цикл это разные типы заданий,знаю стремно,но мне лень переделывать
+        if uuid['isRandom']:
+            random = True
+        if random:
+            results.append('Это задание рандомное! Ответы могут не совпадать!')
         for i in soup.find_all('vim-test-item', attrs={'correct': 'true'}):
             results.append(i.text)
         for i in soup.find_all('vim-input-answers'):            
@@ -74,8 +79,11 @@ def answerparse(taskHash):
                     results.append(f'{f.text} - {i.text}')
         for i in soup.find_all('vim-groups-row'):
             for l in i.find_all('vim-groups-item'):
-                a = base64.b64decode(l['text']) 
-                results.append(f"{a.decode('utf-8')}")   
+                try:
+                    a = base64.b64decode(l['text']) 
+                    results.append(f"{a.decode('utf-8')}")   
+                except:
+                    pass
         for i in soup.find_all('vim-strike-out-item', attrs={'striked': 'true'}):
             results.append(i.text)
         for i in soup.find_all('vim-dnd-image-set-drag'):
@@ -86,23 +94,21 @@ def answerparse(taskHash):
             for f in soup.find_all('vim-dnd-image-drop'):
                 if i['answer-id'] in f['drag-ids']:
                     results.append(f'{f.text} - {i.text}')
-        if uuid['isRandom']:
-            random = True
-            return random
-    if random:
-        print('В тесте есть рандомные задания!')
-
+    return results
                 
-print("Введите комнату")
-taskHash = input()
-answerparse(taskHash)
-i = 0
-for item in results:
-    if 'Вопрос' in item:
-        i = i + 1
-        print(Fore.CYAN + str(i),Fore.MAGENTA + str(item))
-    else:
-        print(Fore.GREEN + str(item))
-
-
+# улучшаю читаемость ответов,буду добавлять.
+def syntaxgood(results):
+    results = ' '.join(results)
+    r = re.compile("sqrt{(.*?)}")
+    bol = re.compile("gt")
+    men = re.compile("lt")
+    for i in r.findall(results):
+        results = results.replace("\sqrt{" + str(i) + "}", "√" + str(i))
+    for i in bol.findall(results):
+        results = results.replace("\gt", ">")
+    for i in men.findall(results):
+        results = results.replace("\lt", "<")
+    results = results.split(' ')
+    
+    return results
 
