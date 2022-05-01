35db0332f0abb44b105840acfa472020e4a801ac
Xartd0 <43171120+xartd0@users.noreply.github.com>
2022-05-01 20:25:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,27 +7,31 @@ import re
 import asyncio
 import aiohttp
 
-token= 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2NDg2NTMzNDUsImV4cCI6MTY1MTI0NTM0NSwicm9sZXMiOlsiUk9MRV9FRFVfU0tZU01BUlRfU1RVREVOVF9VU0FHRSJdLCJhdXRoVXNlcklkIjpudWxsLCJ1c2VySWQiOjU3NjMwMTQ0LCJlbWFpbCI6InNkZmdlNHRlc3RAZ21haWwuY29tIiwibmFtZSI6ImZnaGRmZ2hkZmdoIiwic3VybmFtZSI6ImRmZ2hkZmdoIiwiaWRlbnRpdHkiOiJuYWtvbHVzZW11In0.q49DECee8SLjmxkjKW1bEhFFbPR74xKSO52AnvVvX6BqwM-4ILtraGI8rTpOU_GRUtZczoMhnXXOJoHi3S2S45TJ3XN97mZw7NI1apxQ5CKJoYYrnoUnhG6Hh7bahKJZ6U3es_66t2QFaQngXwWslhMl4UmThsJgCsmqC4d_X3ivxnde-cuQJ5o-quIGoNnBS13QXpVKcULe-zeRlPUJeJITSaitfH-LIkE7B2aGt4l-XET1hXxUavebP7ItUJ2p-J_Y53z8nhqLKYxs2_JJ_3sY8lcO_Gga_gEKUjhzxmoKmTb0Hn9F9LIZ4_OSD0sQjS50mCv0HnhcbAAB1A4P1BvcPnn_FDOhj0LE4tiN75YWcPldSjr7KXBc4udJBic_ir8xxKkEES__F_OM65tQecDaY_Pc6Sn6bb3KVVGMzBYAnZrlQ7J9EdqeWizhZZqg8ebLywPr9XdCy7iUxGN8n2zlxxLSQjOZ06RyZkXZqk-MjYwwYoteBSVcj5Hawc9v73AUAiz-V4p8g8Mo3vge5qJZwExsWTcwN7gOcX_VO22BcrbUkhaH-69zd3RM1JEQ5BnZNXGakvCm-EQlOQL9FoqtXCvmrnleak8G4G1Mw11gldSYpZAlYVNkjWOoYGuw-ajhSuVPaNcP3IxUTppzirjFdbBJ7rnT1bJ6SBQUoQQ'
-
-headers = {
-        'Content-Type': 'application/json',
-        'Authorization': token,
-        'User-Agent':'User-Agent Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0'
-    }
 
 async def get_room(taskHash):
     url = f"https://api-edu.skysmart.ru/api/v1/task/preview"
     payload = "{\"taskHash\":\"" + taskHash + "\"}"
-    headers = headers
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2NTE0MDI2MzMsImV4cCI6MTY1Mzk5NDYzMywicm9sZXMiOlsiUk9MRV9FRFVfU0tZU01BUlRfU1RVREVOVF9VU0FHRSJdLCJhdXRoVXNlcklkIjpudWxsLCJ1c2VySWQiOjM3Nzg4NjQ3LCJlbWFpbCI6InRlc3RpbGluZXRlc3RAZ21haWwuY29tIiwibmFtZSI6ItCf0JDQktCe0JDQotCf0J7QktCi0JDQnyIsInN1cm5hbWUiOiLQq9CS0JDQn9Cr0JLQkNCf0KvQktCQIiwiaWRlbnRpdHkiOiJiYWZlZHVuYWdlIn0.YgHn0lJEYmXLmn3g8vFl7NN5awMrf9S7diDhP3wsu9A_4BLzGY6YQqYHZrt-dfGFRcGUHsmrwTOl-QgTQ_gSspL-NYE3DADs0IzbDrwRpL4LD2rSDM016LH4pS2-TqHknV-KN1lX49kva55JUEQzmpzNkKtJtnGluRaBSX83misEfjYcyM0RNxVSaCP89hP0r-dSd80e8eylvdveh0TrOrEuPQAlN9bBJCHPvGgRlxbGxvi0OBPeBd6_wS_vBf9JP4WIjmNaSi1CV0ddijlDcb42DhNFrmkV8CA_0-R63MCH0DxX5hd5igisd726Yijz_bK_dYkSxbCa0FN_W2mDb41W5xBx2BU7jd2DMz6250B7ceZo_Y3FHSZ9G2EkHVrKuoUxaFXmXUfHOxlcynH2tbUSMBVTW9nZwEzQh8Rmf-p5UuNStmilfqIrLGPz8LQx5_-LBbgusQK6pjbsesCwicsM6KN-jA1E45iqufe1OfebEYzyfEGRVnFwNlCNWuGR5PY0263W-vo-tupzR0UHmjT8PD73WE-UjprTm_gPpfu9ZjCG82jdalrbmZv-d72pEd5YaBW9BSrtw2FvaFHM0IZXfIsP79MjNmXPay68pmlMiSV8bimVzgEBlmek3rC6gLGmNkURN0OQrWWCcoW8g6EMfJ3NcjUVPKi3OajdH5w',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0',
+        'Accept:': 'application/json, text/plain, */*'
+    }
     async with aiohttp.ClientSession() as session:
         async with session.post(url, headers=headers, data=payload) as resp:
             steps_raw = await resp.json() 
+            print(steps_raw)
             await session.close()
             return steps_raw['meta']['stepUuids'] # все uuid заданий
             
 async def get_json_html(uuid):
     url = "https://api-edu.skysmart.ru/api/v1/content/step/load?stepUuid=" + uuid
-    headers = headers
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2NTE0MDI2MzMsImV4cCI6MTY1Mzk5NDYzMywicm9sZXMiOlsiUk9MRV9FRFVfU0tZU01BUlRfU1RVREVOVF9VU0FHRSJdLCJhdXRoVXNlcklkIjpudWxsLCJ1c2VySWQiOjM3Nzg4NjQ3LCJlbWFpbCI6InRlc3RpbGluZXRlc3RAZ21haWwuY29tIiwibmFtZSI6ItCf0JDQktCe0JDQotCf0J7QktCi0JDQnyIsInN1cm5hbWUiOiLQq9CS0JDQn9Cr0JLQkNCf0KvQktCQIiwiaWRlbnRpdHkiOiJiYWZlZHVuYWdlIn0.YgHn0lJEYmXLmn3g8vFl7NN5awMrf9S7diDhP3wsu9A_4BLzGY6YQqYHZrt-dfGFRcGUHsmrwTOl-QgTQ_gSspL-NYE3DADs0IzbDrwRpL4LD2rSDM016LH4pS2-TqHknV-KN1lX49kva55JUEQzmpzNkKtJtnGluRaBSX83misEfjYcyM0RNxVSaCP89hP0r-dSd80e8eylvdveh0TrOrEuPQAlN9bBJCHPvGgRlxbGxvi0OBPeBd6_wS_vBf9JP4WIjmNaSi1CV0ddijlDcb42DhNFrmkV8CA_0-R63MCH0DxX5hd5igisd726Yijz_bK_dYkSxbCa0FN_W2mDb41W5xBx2BU7jd2DMz6250B7ceZo_Y3FHSZ9G2EkHVrKuoUxaFXmXUfHOxlcynH2tbUSMBVTW9nZwEzQh8Rmf-p5UuNStmilfqIrLGPz8LQx5_-LBbgusQK6pjbsesCwicsM6KN-jA1E45iqufe1OfebEYzyfEGRVnFwNlCNWuGR5PY0263W-vo-tupzR0UHmjT8PD73WE-UjprTm_gPpfu9ZjCG82jdalrbmZv-d72pEd5YaBW9BSrtw2FvaFHM0IZXfIsP79MjNmXPay68pmlMiSV8bimVzgEBlmek3rC6gLGmNkURN0OQrWWCcoW8g6EMfJ3NcjUVPKi3OajdH5w',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0',
+        'Accept:': 'application/json, text/plain, */*'
+    }
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as resp:
             answer_row = await resp.json()
@@ -105,6 +109,7 @@ async def answerparse(taskHash):
             for f in soup.find_all('vim-dnd-image-drop'):
                 if i['answer-id'] in f['drag-ids']:
                     results.append(f'{f.text} - {i.text}')
+    
     return results
 
 async def ochistka(string):
@@ -162,3 +167,5 @@ async def ochistka(string):
     for i in besk.findall(string):
         string = string.replace(r"\infty", "∞")
     return string
+
+
