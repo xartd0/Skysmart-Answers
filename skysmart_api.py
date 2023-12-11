55c4a91d0b54976a2f73a1f76591193b91d29c3b
Xartd0 <43171120+xartd0@users.noreply.github.com>
2023-12-11 22:43:47 +0300

    Update skysmart_api.py

--- skysmart_api.py
+++ skysmart_api.py
@@ -3,57 +3,70 @@ from user_agent import generate_user_agent
 from utils import api_variables as api
 from utils import config
 
-
-async def get_headers():
-    """---- Получаем jwt токен при входе в аккаунт skysmart ----"""
+async def get_headers(session):
+    """Get jwt token for Skysmart account login."""
     user_agent = generate_user_agent()
 
-    async with aiohttp.ClientSession() as session:
+    try:
         async with session.post(api.url_auth, headers={'User-Agent': user_agent}, data=config.auth_creds) as resp:
-            
-            json_resp = await resp.json() 
+            if resp.status == 200:
+                json_resp = await resp.json()
+                return {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Authorization': 'Bearer ' + json_resp['jwtToken'],
+                    'Connection': 'keep-alive',
+                    'Content-Type': 'application/json',
+                    'User-Agent': user_agent,
+                }
+            else:
+                raise Exception(f"Auth request failed with status: {resp.status}")
+    except Exception as e:
+        print(f"Error during authentication: {e}")
+        return None
 
-            return {
-                'Accept' : 'application/json, text/plain, */*',
-                'Authorization': 'Bearer ' + json_resp['jwtToken'],
-                'Connection' : 'keep-alive',
-                'Content-Type': 'application/json',
-                'User-Agent': user_agent,
-            }
-
-async def get_room(taskHash):
-    """---- Получаем uuid всех заданий в тесте ----"""
+async def get_room(taskHash, session):
+    """Get uuid for all tasks in a test."""
     payload = "{\"taskHash\":\"" + taskHash + "\"}"
-    headers = await get_headers()
+    headers = await get_headers(session)
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post(api.url_room, headers=headers, data=payload) as resp:
+    if headers is not None:
+        try:
+            async with session.post(api.url_room, headers=headers, data=payload) as resp:
+                if resp.status == 200:
+                    steps_raw = await resp.json()
+                    return steps_raw['meta']['stepUuids']
+                else:
+                    raise Exception(f"Room request failed with status: {resp.status}")
+        except Exception as e:
+            print(f"Error during getting room: {e}")
 
-            steps_raw = await resp.json() 
-            await session.close()
-
-            return steps_raw['meta']['stepUuids'] 
-        
-
-async def get_meta(taskHash):
+async def get_meta(taskHash, session):
+    """Get metadata for a task."""
     payload = "{\"taskHash\":\"" + taskHash + "\"}"
-    headers = await get_headers()
+    headers = await get_headers(session)
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post(api.url_room, headers=headers, data=payload) as resp:
-            steps_raw = await resp.json() 
-            await session.close()
-            return steps_raw["title"],steps_raw["meta"]["path"]["module"]["title"]
-        
-            
-async def get_task_html(uuid):
-    """---- Получаем html по uuid каждого задания отдельно ----"""
-    headers = await get_headers()
+    if headers is not None:
+        try:
+            async with session.post(api.url_room, headers=headers, data=payload) as resp:
+                if resp.status == 200:
+                    steps_raw = await resp.json()
+                    return steps_raw["title"], steps_raw["meta"]["path"]["module"]["title"]
+                else:
+                    raise Exception(f"Meta request failed with status: {resp.status}")
+        except Exception as e:
+            print(f"Error during getting meta: {e}")
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(api.url_steps + uuid, headers=headers) as resp:
+async def get_task_html(uuid, session):
+    """Get HTML for a task by uuid."""
+    headers = await get_headers(session)
 
-            answer_row = await resp.json()
-            await session.close()
-
-    return answer_row['content']
+    if headers is not None:
+        try:
+            async with session.get(api.url_steps + uuid, headers=headers) as resp:
+                if resp.status == 200:
+                    answer_row = await resp.json()
+                    return answer_row['content']
+                else:
+                    raise Exception(f"Task HTML request failed with status: {resp.status}")
+        except Exception as e:
+            print(f"Error during getting task HTML: {e}")
