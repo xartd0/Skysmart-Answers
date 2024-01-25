aff2d8b9a0988447d10fa4e2ea8c96c577989a80
Xartd0 <43171120+xartd0@users.noreply.github.com>
2024-01-25 20:40:13 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,8 +1,10 @@
 @echo off
 echo Установка необходимых библиотек...
+
 pip install aiohttp
 pip install beautifulsoup4
 pip install user_agent
 pip install base64
+pip install uvicorn
 
 echo Установка завершена!
