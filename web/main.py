1ef028b9d9e6346941721e698944f039db573327
xartd0 <lordpasha05@gmail.com>
2024-03-12 18:59:08 +0300

    поправил web

--- web/main.py
+++ web/main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from parser.answer_module import SkyAnswers
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="web/static"), name="static")
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,9 +21,19 @@ app.add_middleware(
 class RoomRequest(BaseModel):
     roomName: str
 
+@app.get("/", response_class=HTMLResponse)
+async def index():
+    with open("web/templates/index.html", "r", encoding="utf-8") as f:
+        return f.read()
+
+
 @app.post("/get_answers/")
 async def get_answers(request: RoomRequest):
     answers_module = SkyAnswers(request.roomName)
     answers = await answers_module.get_answers()
     more_info = await answers_module.get_room_info()
     return answers, more_info
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("web.main:app", host="127.0.0.1", port=8000, reload=True)
